641bfbca8972744b6d86a1d95814b67483e8be0c
Will Chen <chenwill98@gmail.com>
2025-01-18 00:41:40 -0500

    Finished tutorial

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
+export const runtime = 'edge';
+export const preferredRegion = 'home';
+export const dynamic = 'force-dynamic';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
